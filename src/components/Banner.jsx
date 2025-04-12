c125f0aaffbff0b21857e5ab5b1d40f287476e30
parvej-shah <parvejshahlabib007@gmail.com>
2025-04-12 19:41:02 +0600

    Refactor code structure for improved readability and maintainability and changed img for better load time

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import Typewriter from 'typewriter-effect';
 import CVButton from './CVButton';
-import bannerImg from '../assets/images/banner.jpg';
+import bannerImg from '../assets/images/banner.webp';
 import SocialLinks from './SocialLinks';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
@@ -12,7 +12,7 @@ const HeroSection = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         {/* Right Side Image */}
         <div data-aos="fade-left" data-aos-duration="5000" className="avatar">
-          <div className="w-80 rounded-full ring ring-offset-base-100 ring-offset-2">
+          <div className="w-96 rounded-full ring ring-offset-base-100 ring-offset-2">
             <img
               src={bannerImg}
               alt="Profile"
@@ -28,16 +28,16 @@ const HeroSection = () => {
           <h2 className="text-4xl mt-4 font-medium text-secondary">
             <Typewriter
               options={{
-                strings: ['Frontend Developer','React Developer', 'MERN Enthusiast'],
+                strings: ['Front-End Developer','React Specialist','Problem Solver'],
                 autoStart: true,
                 loop: true,
               }}
             />
           </h2>
           <p className="py-6 text-gray-600">
-          I love to create seamless digital experiences through responsive websites and modern web applications. Let’s turn your dream project into reality!
+          Crafting elegant interfaces with React, where every component sings, every pixel dances, and every line of code tells a story of precision and care. Explore my work — let’s build something beautiful together.
           </p>
-          <div className="flex gap-4 justify-center lg:justify-start">
+          <div className="flex gap-4 justify-center mb-2 lg:justify-start">
             <CVButton/>
             <button className="btn btn-outline ">Contact Me</button>
           </div>
